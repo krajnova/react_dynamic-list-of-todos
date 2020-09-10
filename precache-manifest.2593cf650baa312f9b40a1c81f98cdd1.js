85d5cb3b886c9cb009f7b15b85170283ca599c31
Anatasia <krajnova.stasy@gmail.com>
2020-09-10 19:31:23 +0300

    Updates

---
+++ precache-manifest.2593cf650baa312f9b40a1c81f98cdd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8dea7c7dc12787a23967151a6d907d0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "49688374242b56dfce94",
+    "url": "./static/css/main.77b5c301.chunk.css"
+  },
+  {
+    "revision": "c5622039e64d010c4fef",
+    "url": "./static/js/2.b5cea842.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.b5cea842.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49688374242b56dfce94",
+    "url": "./static/js/main.6ecb8b19.chunk.js"
+  },
+  {
+    "revision": "0c165204287428b65967",
+    "url": "./static/js/runtime-main.a37292e9.js"
+  }
+]);
